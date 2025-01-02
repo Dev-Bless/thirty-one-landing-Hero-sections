37579e605b80cd9e0b855932c4f78f6dcab4a7f7
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-01-02 19:47:43 +0000

    implementation of landing page 2

--- src/app/pages/landing-page-2/landing-page-2.component.ts
+++ src/app/pages/landing-page-2/landing-page-2.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-landing-page-2',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './landing-page-2.component.html',
   styleUrl: './landing-page-2.component.css'
 })
 export class LandingPage2Component {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
