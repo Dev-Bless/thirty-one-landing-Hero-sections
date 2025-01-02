4799a544b563be13454f010eef2e6264f7767bea
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-01-02 16:20:53 +0000

    Initial commit

---
+++ src/app/pages/landing-page-1/landing-page-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-1',
+  imports: [],
+  templateUrl: './landing-page-1.component.html',
+  styleUrl: './landing-page-1.component.css'
+})
+export class LandingPage1Component {
+
+}
