4243622b8cc2819ab368192548a0ef318ad39394
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-01-02 18:23:42 +0000

    final landing page 1

--- src/app/pages/landing-page-1/landing-page-1.component.ts
+++ src/app/pages/landing-page-1/landing-page-1.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-landing-page-1',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './landing-page-1.component.html',
   styleUrl: './landing-page-1.component.css'
 })
 export class LandingPage1Component {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
