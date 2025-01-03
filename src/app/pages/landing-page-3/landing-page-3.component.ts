ac0531ccf3bc68ff07c179c1a1a1f9a137dc0255
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-01-03 12:53:28 +0000

    implementation of landing page 3

--- src/app/pages/landing-page-3/landing-page-3.component.ts
+++ src/app/pages/landing-page-3/landing-page-3.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-landing-page-3',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './landing-page-3.component.html',
   styleUrl: './landing-page-3.component.css'
 })
 export class LandingPage3Component {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
